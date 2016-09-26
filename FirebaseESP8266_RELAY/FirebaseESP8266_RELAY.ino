f52a1ec8cb3bec58cfa8e654e4bb09184ce4651c
Aleksey Kudakov <akudakov@intellimec.com>
2016-09-26 18:48:53 +0300

    Relay Demo App

---
+++ FirebaseESP8266_RELAY/FirebaseESP8266_RELAY.ino
@@ -0,0 +1,60 @@
+#include <FirebaseArduino.h>
+#include <ESP8266WiFi.h>
+
+#define FIREBASE_HOST "gdg-iot-demo-app.firebaseio.com"
+#define WIFI_SSID "MERA-BELKIN"
+#define WIFI_PASSWORD "Mayak4!5WeMo"
+
+#define RELAY_PIN 5
+bool pinState = false;
+
+void setup() {
+  Serial.begin(115200);
+
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+
+  Firebase.begin(FIREBASE_HOST);
+  Firebase.stream("/switch/" + WiFi.macAddress());
+
+  pinMode(RELAY_PIN, OUTPUT);
+}
+
+
+void loop() {
+  if (Firebase.failed()) {
+    Serial.println("streaming error");
+    Serial.println(Firebase.error());
+  }
+
+  if (Firebase.available()) {
+    FirebaseObject event = Firebase.readEvent();
+    String eventType = event.getString("type");
+    eventType.toLowerCase();
+
+    Serial.print("event: ");
+    Serial.println(eventType);
+    if (eventType == "put") {
+      pinState = event.getBool("data");
+
+      Serial.print("path: ");
+      Serial.print(event.getString("path"));
+      Serial.print(", data: ");
+      Serial.println(pinState);
+    }
+  }
+
+  if (pinState) {
+    digitalWrite(RELAY_PIN, HIGH);
+  } else {
+    digitalWrite(RELAY_PIN, LOW);
+  }
+}
